680582bbccce3fe5ab767240fe7a0ce9a1b388b9
ngocdeptrai <nguyenngoc02082000@gmail.com>
2022-07-21 16:10:40 +0700

    ngoc commit

--- src/Redux/Home/HomeSlice.js
+++ src/Redux/Home/HomeSlice.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const HomeSlice = () => {
-  return (
-    <div>HomeSlice</div>
-  )
-}
+  return <div>HomeSlice</div>;
+};
 
-export default HomeSlice
+// export default HomeSlice
